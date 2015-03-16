ba15c1ce6c4d48f7f356ca371dbbbd28235fe65d
Chris Loew <cloew123@gmail.com>
2015-03-16 14:46:49 -0600

    Some slight tidying of the knot window class

--- src/knot/core/painters/window_painter.py
+++ src/knot/core/painters/window_painter.py
@@ -1,10 +1,11 @@
 from PySide.QtGui import QMainWindow
 
 class WindowPainter:
-    """ Handles creation of the Qt widget to reresent the window """
+    """ Handles creation of the Qt widget to represent the window """
     
     def __init__(self, content):
         """ Initialize the Painter with its internal content """
+        pass
         
     def draw(self):
         """ Draw the Text Painter """
