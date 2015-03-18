906977ff5b8020e7cca8cb1e9eddfcefeb4b60b7
Chris Loew <cloew123@gmail.com>
2015-03-18 17:45:50 -0600

    Can now load the tokens from a file

---
+++ src/knot/loader/token/token_factory.py
@@ -0,0 +1,46 @@
+from .knot_scope_detector import KnotScopeDetector
+from .content_token import ContentToken
+from .widget_token import WidgetToken
+
+from knot.widget_factory import HasWidgetType
+
+from kao_file import KaoFile, SectionFinder
+
+class TokenFactory:
+    """ Represents a tokenized widget from a knot file """
+    
+    def loadAllTokens(self, lines):
+        """ Load and return all Widget Tokens at the same depth from the given lines """
+        file = KaoFile(lines)
+        sectionFinder = SectionFinder(KnotScopeDetector())
+        section = None
+        nextSectionStartsOn = 0
+        
+        tokens = []
+        
+        while True:
+            section = sectionFinder.find(file, startAt=nextSectionStartsOn)
+            if section is None:
+                break
+            else:
+                tokens.append(self.load(section))
+                if file.getLineAt(section.endIndex).isLastLine():
+                    break
+                else:
+                    nextSectionStartsOn = file.getLineAt(section.endIndex).next().lineIndex
+                
+        return tokens
+    
+    def load(self, section):
+        """ Load a token from the given section """
+        if self.isWidget(section):
+            return WidgetToken(section, self)
+        else:
+            return ContentToken(section)
+        
+    def isWidget(self, section):
+        """ Return if the given section if for a Widget """
+        firstLine = section[0]
+        pieces = firstLine.split()
+        return len(pieces) == 1 and HasWidgetType(pieces[0].strip())
+            
