56bb1ca50287f3bfc625dfd7231f2a43e5df8af4
Chris Loew <cloew123@gmail.com>
2015-03-18 17:44:26 -0600

    Added Section Detector to detect a Knot Scope

---
+++ src/knot/loader/token/knot_scope_detector.py
@@ -0,0 +1,22 @@
+from pyfiles import LeadingWhitespace
+    
+class KnotScopeDetector:
+    """ Detects the section of a knot file defining a new scope """
+    
+    def __init__(self, startingLine=None):
+        """ Initialize the function detector """
+        self.startingLine = startingLine
+    
+    def isStart(self, line):
+        """ Return if the given line is the start of a Knot Scope """
+        isStart = line.strip() != ''
+        if isStart:
+            self.startingLine = line
+        return isStart
+        
+    def isEnd(self, line):
+        """ Returns if the given line is the end of a Knot Scope """
+        if line.isLastLine():
+            return True
+        else:
+            return LeadingWhitespace(line.next()) <= LeadingWhitespace(self.startingLine)
