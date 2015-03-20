a417fa09bd7122f8e90c1dc2435a33ce947d32ba
Chris Loew <cloew123@gmail.com>
2015-03-20 11:08:48 -0600

    The Center Positioning policy now uses the widget tracker

--- src/knot/core/positioning/center.py
+++ src/knot/core/positioning/center.py
@@ -1,24 +1,29 @@
 from knot.events.event_group import EventGroup
-from knot.events.event_types import *
+from knot.events.event_types import DISPLAYED, RESIZED
+from knot.events.tracker.widget_tracker import WidgetTracker
 
 class Center:
     """ Modification that ensures its widget is centered in its parent """
     
     def __init__(self):
         """ Initialize the Centerer """
+        self.widgetTracker = WidgetTracker([DISPLAYED, RESIZED], self.reposition)
         
     def applyToWidget(self, widget):
         """ Apply the policy to the neighbor """
         self.widget = widget
-        self.widget.on(DISPLAYED, self.positionWidget)
-        self.widget.on(RESIZED, self.positionWidget)
+        self.widgetTracker.apply(widget)
         self.widget.parent.on(RESIZED, self.positionWidget)
     
     def positionWidget(self, aWidget, event):
         """ Center the widget in its parent """
-        parent = self.widget.parent
+        self.reposition(self.widget)
+        
+    def reposition(self, widget):
+        """ Reposition the widget """
+        parent = widget.parent
         centerx = parent.width/2
         centery = parent.height/2
         
-        self.widget.left = centerx - self.widget.width/2
-        self.widget.top = centery - self.widget.height/2
+        widget.left = centerx - widget.width/2
+        widget.top = centery - widget.height/2
