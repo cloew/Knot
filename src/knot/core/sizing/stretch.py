fd1fd6a17b0e34fa6242a82d57e78ce0d3b82399
Chris Loew <cloew123@gmail.com>
2015-03-20 16:09:13 -0600

    Updated Stretch Sizing Policy to use the Sibling Tracker

--- src/knot/core/sizing/stretch.py
+++ src/knot/core/sizing/stretch.py
@@ -1,37 +1,22 @@
 from knot.events.event_group import EventGroup
 from knot.events.event_types import *
+from knot.events.tracker.siblings_tracker import SiblingsTracker
 
 class Stretch:
     """ Size a widget by letting it take up as much space as needed """
     
+    def __init__(self):
+        """ Initialize the Centerer """
+        self.siblingsTracker = SiblingsTracker([MOVED, RESIZED], self.resize)
+    
     def applyToWidget(self, widget):
         """ Apply the policy to the neighbor """
-        self.widget = widget
+        self.siblingsTracker.apply(widget)
         
-        self.siblingToResizeEventGroup = {}
-        self.addResizeEvents(widget.siblings)
-        self.childrenEventGroup = EventGroup(widget.parent, [CHILD_ADDED], self.trackNewSibling)
-        self.childrenEventGroup.register()
-        
-    def resize(self, aWidget, event):
+    def resize(self, widget):
         """ Adjust the given widget so it is sized properly """
-        siblings = self.widget.siblings
+        siblings = widget.siblings
         requiredWidth = sum([sibling.width for sibling in siblings])
         
-        width = self.widget.parent.width - requiredWidth
-        self.widget.resize(width, self.widget.parent.height)
-        
-    def addResizeEvents(self, siblings):
-        """ Add Resizing Events to the siblings """
-        for sibling in siblings:
-            self.addSiblingResizeEvent(sibling)
-            
-    def addSiblingResizeEvent(self, sibling):
-        """ Add resizing events to the given sibling """
-        eventGroup = EventGroup(sibling, [MOVED, RESIZED], self.resize)
-        self.siblingToResizeEventGroup[sibling] = eventGroup
-        eventGroup.register()
-            
-    def trackNewSibling(self, parent, sibling=None):
-        """ Track the new sibling that was added """
-        self.addSiblingResizeEvent(sibling)
+        width = widget.parent.width - requiredWidth
+        widget.resize(width, widget.parent.height)
