de0f8c490908e3c0804433dae802dd698ace6803
Chris Loew <cloew123@gmail.com>
2015-03-20 12:29:14 -0600

    Center positioning now uses the parent tracker

--- src/knot/core/positioning/center.py
+++ src/knot/core/positioning/center.py
@@ -1,5 +1,7 @@
 from knot.events.event_group import EventGroup
 from knot.events.event_types import DISPLAYED, RESIZED
+
+from knot.events.tracker.parent_tracker import ParentTracker
 from knot.events.tracker.widget_tracker import WidgetTracker
 
 class Center:
@@ -8,16 +10,13 @@ class Center:
     def __init__(self):
         """ Initialize the Centerer """
         self.widgetTracker = WidgetTracker([DISPLAYED, RESIZED], self.reposition)
+        self.parentTracker = WidgetTracker([RESIZED], self.reposition)
         
     def applyToWidget(self, widget):
         """ Apply the policy to the neighbor """
         self.widget = widget
         self.widgetTracker.apply(widget)
-        self.widget.parent.on(RESIZED, self.positionWidget)
-    
-    def positionWidget(self, aWidget, event):
-        """ Center the widget in its parent """
-        self.reposition(self.widget)
+        self.parentTracker.apply(widget)
         
     def reposition(self, widget):
         """ Reposition the widget """
