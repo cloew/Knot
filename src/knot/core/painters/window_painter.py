715c5d2712da79faee440e67f7ebaf0a3afab2a2
Chris Loew <cloew123@gmail.com>
2015-03-24 10:32:27 -0600

    Added Sizing Handler that does nothing to let the window stay at the proper size

--- src/knot/core/painters/window_painter.py
+++ src/knot/core/painters/window_painter.py
@@ -1,10 +1,11 @@
 from .container_painter import ContainerPainter
+from ..sizing.do_nothing import DoNothing
 
 from PySide.QtGui import QMainWindow
 
 class WindowPainter(ContainerPainter):
     """ Handles creation of the Qt widget to represent the window """
-    DEFAULT_SIZING = None
+    DEFAULT_SIZING = DoNothing()
         
     def buildQWidget(self, widget):
         """ Draw the Main Window and tell it to maximize """
---
+++ src/knot/core/sizing/do_nothing.py
@@ -0,0 +1,11 @@
+
+class DoNothing:
+    """ Represents a sizing method that does nothing """
+    
+    def applyToWidget(self, widget):
+        """ Apply the policy to the neighbor """
+        pass
+        
+    def resize(self, widget):
+        """ Adjust the given widget so it is sized properly """
+        pass
