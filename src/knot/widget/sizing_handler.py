1997a512d435e0adbf29f09816b62cf79fc7fb5a
Chris Loew <cloew123@gmail.com>
2015-03-24 11:20:17 -0600

    Removed dead code

--- src/knot/widget/sizing_handler.py
+++ src/knot/widget/sizing_handler.py
@@ -8,15 +8,3 @@ class SizingHandler(PoliciesHandler):
     def getDefaultPolicy(self, dimension=BOTH):
         """ Return the default policy to be used for children """
         return [self.widget.painter.DEFAULT_SIZING[dimension]]
-    
-    # def __init__(self, widget, policy):
-        # """ Initialize the Handler with the widget and its sizing policy """
-        # self.widget = widget
-        # if policy is None:
-            # policy = widget.painter.DEFAULT_SIZING
-        # self.policy = policy
-        
-    # def apply(self):
-        # """ Apply the positionig policy """
-        # if self.policy is not None:
-            # self.policy.applyToWidget(self.widget)
