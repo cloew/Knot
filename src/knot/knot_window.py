ef08412159d8a6830f57693e2e206dd06ad56f8f
Chris Loew <cloew123@gmail.com>
2015-03-25 14:31:09 -0600

    Tidied up the Knot Window

--- src/knot/knot_window.py
+++ src/knot/knot_window.py
@@ -1,10 +1,7 @@
-from .widget import Widget
-from .events.event_types import *
-from .loader.factory.widget_factory import WidgetFactory
+from knot.dimensions import HORIZONTAL, VERTICAL
+from .widget.widget import Widget
 
 from .core.painters.window_painter import WindowPainter
-from .core.positioning.center import Center
-from .core.sizing.stretch import Stretch
 
 from kao_decorators import proxy_for
 
@@ -15,22 +12,11 @@ class KnotWindow(Widget):
     def __init__(self):
         """ Initialize the window """
         Widget.__init__(self, WindowPainter(''))
-        # self.div = WidgetFactory.build('div', "", positioning=Center(), sizing=Stretch())
-        # self.addChild(self.div)
         
-        # self.label1 = WidgetFactory.build('text', "Some Text")
-        # self.div.addChild(self.label1)
-        # self.label2 = WidgetFactory.build('text', "Some Other Text", sizing=Stretch())
-        # self.div.addChild(self.label2)
-        # self.label3 = WidgetFactory.build('text', "EVEN MORE TEXT!!!!!!")
-        # self.div.addChild(self.label3)
-        
-        # self.label2.on(RESIZED, self.printSomething)
-        
-    def printSomething(self, div, event):
-        print("Inside event")
-        print(event)
-        print(self.div, self.div._qwidget.geometry())
-        print(self.label1, self.label1._qwidget.geometry())
-        print(self.label2, self.label2._qwidget.geometry())
-        print(self.label3, self.label3._qwidget.geometry())
+    def printChildren(self, parent=None):
+        """ """
+        if parent is None:
+            parent = self
+        for child in parent.children:
+            print(child, child._qwidget.geometry())
+            self.printChildren(parent=child)
