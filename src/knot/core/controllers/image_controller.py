d23f2079cc71f03269b3009f0daceafba4826ff0
Chris Loew <cloew123@gmail.com>
2015-03-31 11:39:16 -0600

    Added basic controller for the Image

---
+++ src/knot/core/controllers/image_controller.py
@@ -0,0 +1,7 @@
+
+class ImageController:
+    """ Handle the image to determine the file to load """
+    
+    def __init__(self, filename):
+        """ Initialize the controller with the image to load """
+        self.filename = filename
