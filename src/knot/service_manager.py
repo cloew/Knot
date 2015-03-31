ad5805aabcd89499811994d1b088e1b9bf02228b
Chris Loew <cloew123@gmail.com>
2015-03-31 13:49:30 -0600

    Added Smart Default Provider to provide the relevant Knot Service

--- src/knot/service_manager.py
+++ src/knot/service_manager.py
@@ -15,4 +15,11 @@ class ServiceManager:
         """ Register the given service class """
         self.nameToClass[name] = serviceCls
         
+    def getService(self, name): 
+        """ Return the requested service """
+        if name not in self.nameToService:
+            self.addService(name, self.nameToClass[name])
+            
+        return self.nameToService[name]
+        
 ServiceManager = ServiceManager()
