e9f073c6a205f1fb9233c5e44c598e6b21d513ca
Chris Loew <cloew123@gmail.com>
2015-04-01 10:40:37 -0600

    The painter class is now optional and tidied up how the namespaced classes are used

--- src/knot/loader/config/widget_config.py
+++ src/knot/loader/config/widget_config.py
@@ -4,20 +4,29 @@ from kao_modules import NamespacedClass
 class WidgetConfig:
     """ Represents the configuration for a widget """
     
-    def __init__(self, name, painterClassname, controllerClassname=None):
+    def __init__(self, name, painterClassname=None, controllerClassname=None):
         """ Initialize the widget config with its name and the painter classname """
         self.name = name
         self.controllerClassname = controllerClassname
         self.painterClassname = painterClassname
-        self.namespacedPainterClass = NamespacedClass(self.painterClassname)
-        self.namespacedControllerClass = None if controllerClassname is None else NamespacedClass(self.controllerClassname)
+        
+        self.namespacedPainterClass = self.tryToBuildNamespacedClass(painterClassname)
+        self.namespacedControllerClass = self.tryToBuildNamespacedClass(controllerClassname)
         
     def build(self, content, *args, positioning=None, sizing=None, **kwargs):
         """ Return the proper widget object """
-        controller = None if self.namespacedControllerClass is None else self.namespacedControllerClass.instantiate(*args, **kwargs)
-        painter = self.namespacedPainterClass.instantiate(content, controller)
+        controller = self.tryToIntantiateClass(self.namespacedControllerClass, *args, **kwargs)
+        painter = self.tryToIntantiateClass(self.namespacedPainterClass, content, controller)
         return Widget(painter, controller=controller, positioning=positioning, sizing=sizing)
         
     def __repr__(self):
         """ Return the string representation of the config """
         return "<WidgetConfig({0}, {1}, {2})>".format(self.name, self.painterClassname, self.controllerClassname)
+        
+    def tryToBuildNamespacedClass(self, classname):
+        """ Build the Namespaced Class object for the given name if the name is not None """
+        return None if classname is None else NamespacedClass(classname)
+        
+    def tryToIntantiateClass(self, namespacedClass, *args, **kwargs):
+        """ Instantiate the Namespaced Class object for the given name if the name is not None """
+        return None if namespacedClass is None else namespacedClass.instantiate(*args, **kwargs)
