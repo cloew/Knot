fd21c38377c6a474d397fa9d62421b0de72b69d8
Chris Loew <cloew123@gmail.com>
2015-04-02 13:44:44 -0600

    Updated Content Token so it can properly handle multiple scoped values

--- src/knot/loader/token/content_token.py
+++ src/knot/loader/token/content_token.py
@@ -1,10 +1,8 @@
 from .token_roles import CONTENT
-from .value.content_value import ContentValue
 from .value.scope_value import ScopeValue
 
 from kao_decorators import proxy_for
 
-@proxy_for('value', ['getValue'])
 class ContentToken:
     """ Represents a widget's content from a knot file """
     ROLE = CONTENT
@@ -16,12 +14,21 @@ class ContentToken:
     
     def __init__(self, section):
         """ Initialize the Content Token """
+        self.valueTokens = []
         text = section[0].strip()
-        self.value = None
-        if ScopeValue.isValidFor(text):
-            self.value = ScopeValue(text)
-        else:
-            self.value = ContentValue(text)
+        self.text = ' '.join([self.handle(piece) for piece in text.split()])
+            
+    def handle(self, piece):
+        """ Handle the piece by storing the value token and return the proper placeholder string """
+        text = piece
+        if ScopeValue.isValidFor(piece):
+            self.valueTokens.append(ScopeValue(piece))
+            text = '{}'
+        return text
+        
+    def getText(self, scope):
+        """ Return the text """
+        return self.text.format(*[value.getValue(scope) for value in self.valueTokens])
         
     def __repr__(self):
         return "<ContentToken:{0}>".format(self.value)
