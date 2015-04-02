675d41419ba503a3f4ebf573a126836a6eeb8e4f
Chris Loew <cloew123@gmail.com>
2015-04-02 13:42:19 -0600

    Updated the button painter so it uses the default policies helper class

--- src/knot/forms/painters/button_painter.py
+++ src/knot/forms/painters/button_painter.py
@@ -1,4 +1,5 @@
 from knot.dimensions import HORIZONTAL, VERTICAL, BOTH
+from knot.policy.default_policies_via_class import DefaultPoliciesViaClass
 from knot.core.painters.painter import Painter
 from knot.core.sizing.use_size_hint import UseSizeHint
 
@@ -6,9 +7,7 @@ from PySide.QtGui import QPushButton
 
 class ButtonPainter(Painter):
     """ Handles creation of the Qt widget for drawing a button """
-    DEFAULT_SIZING = {BOTH:      UseSizeHint(),
-                      HORIZONTAL:UseSizeHint(dimension=HORIZONTAL),
-                      VERTICAL:  UseSizeHint(dimension=VERTICAL)}
+    DEFAULT_SIZING = DefaultPoliciesViaClass(UseSizeHint)
     
     def __init__(self, content, controller=None):
         """ Initialize the Painter with its internal content """
