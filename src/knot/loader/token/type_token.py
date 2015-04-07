7a632ebeab26d7a6472b9261ca4dd4210370a90f
Chris Loew <cloew123@gmail.com>
2015-04-07 13:09:47 -0600

    The Type TOken can now accept keyword arguments

--- src/knot/loader/token/type_token.py
+++ src/knot/loader/token/type_token.py
@@ -13,20 +13,32 @@ class TypeToken:
         """ Initialize the type """
         pieces = text.split('(', 1)
         self.type = pieces[0].strip()
-        self.args = self.getArguments(''.join(pieces[1:]))
+        self.args, self.kwargs = self.getArguments(''.join(pieces[1:]))
         
     def getArguments(self, argumentText):
         """ Return the arguments """
         argumentText = argumentText.split(')')[0]
-        return [ValueFactory.build(arg.strip()) for arg in KnotListParser().parse(argumentText) if arg.strip() != '']
+        argPieces = [arg.strip() for arg in KnotListParser().parse(argumentText) if arg.strip() != '']
+        
+        args = [ValueFactory.build(arg) for arg in argPieces if '=' not in arg]
+        kwargs = {}
+        for arg in argPieces:
+            if '=' in arg:
+                keyword, valueText = arg.split('=', maxsplit=1)
+                kwargs[keyword] = ValueFactory.build(valueText)
+        return args, kwargs
         
     def getArgumentValues(self, scope):
         """ Return the argument values """
         return [arg.getValue(scope) for arg in self.args]
         
+    def getKeywordArgumentValues(self, scope):
+        """ Return the keyword argument values """
+        return {keyword:self.kwargs[keyword].getValue(scope) for keyword in self.kwargs}
+        
     def build(self, factory, scope):
         """ Build this type form the factory """
-        return factory.build(self.type, *self.getArgumentValues(scope))
+        return factory.build(self.type, *self.getArgumentValues(scope), **self.getKeywordArgumentValues(scope))
         
     def __repr__(self):
         return "<TypeToken:{0}:{1}>".format(self.type, self.args)
