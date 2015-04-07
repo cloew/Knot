7a667696e7c5fed6a1a789f005aa406caa6972bb
Chris Loew <cloew123@gmail.com>
2015-04-06 20:33:12 -0600

    The option controller can now have its content specified. If none is specified its content defautls to its value as a string

--- src/knot/forms/controllers/option_controller.py
+++ src/knot/forms/controllers/option_controller.py
@@ -9,12 +9,20 @@ class OptionController:
     def __init__(self, value):
         """ Initialize the Controller with its value """
         self.textChanged = Signal()
+        self.widget = None
         self.app.watch(self, 'text', self.fireSignal)
+        
+    def attachWidget(self, widget):
+        """ Attach the widget """
+        self.widget = widget
         
     @property
     def text(self):
         """ Return the text for this option """
-        return self.value
+        text = self.value
+        if self.widget is not None and self.widget.content is not None:
+            text = self.widget.content.text
+        return text
         
     def fireSignal(self, text):
         """ Fire the text changed signal """
