a2225ea01dec2f664cedcd5631bc6e7a69422fc3
Chris Loew <cloew123@gmail.com>
2015-04-08 11:22:46 -0600

    The file dialog controller now operates on a model

--- src/knot/forms/controllers/file_dialog_controller.py
+++ src/knot/forms/controllers/file_dialog_controller.py
@@ -1,13 +1,15 @@
-from knot import has_scope
+from knot import has_scope, apply_knot_bindings, TwoWayBinding
 from PySide.QtGui import QFileDialog
 
 @has_scope
 class FileDialogController:
     """ Handles opening a file dialog """
+    filename = TwoWayBinding("model")
     
-    def __init__(self):
-        """ Initialize the controller with the *** """
-        self.filename = ''
+    @apply_knot_bindings
+    def __init__(self, filename):
+        """ Initialize the controller with the filename """
+        pass
         
     def openFileDialog(self):
         """ Open the file dialog """
