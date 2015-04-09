753f7849ddb1e9307922e207d97319859ec26d07
Chris Loew <cloew123@gmail.com>
2015-04-09 15:30:19 -0600

    The image controller can now be given a scope value and will update the image according to the changes in the value

--- src/knot/core/controllers/image_controller.py
+++ src/knot/core/controllers/image_controller.py
@@ -1,4 +1,4 @@
-from knot import KnotService
+from knot import KnotService, OneWayBinding, apply_knot_bindings
 from knot.events.event_types import WIDGET_CREATED
 
 from PySide.QtGui import QPixmap
@@ -7,21 +7,31 @@ import os
 
 class ImageController:
     """ Handle the image to determine the file to load """
+    app = KnotService('app')
     fileLoader = KnotService('fileLoader')
+    _filename = OneWayBinding('_filename')
     
-    def __init__(self, filename):
+    @apply_knot_bindings
+    def __init__(self, _filename):
         """ Initialize the controller with the image to load """
-        if os.path.isfile(filename):
-            self.filename = filename
-        else:
-            self.filename = self.fileLoader.getProperPath(filename)
         
     def attachWidget(self, widget):
         """ Attach the widget """
         self.widget = widget
         widget.on(WIDGET_CREATED, self.setImage)
+        self.app.watch(self, '_filename', self.setImage)
         
     def setImage(self, widget=None, event=None):
         """ Set the image for the underlying qt widget """
         pixmap = QPixmap(self.filename)
         self.widget._qwidget.setPixmap(pixmap)
+        self.widget.resize()
+       
+    @property
+    def filename(self):
+        """ Return the proper filename """
+        if os.path.isfile(self._filename):
+            return self._filename
+        else:
+            return self.fileLoader.getProperPath(self._filename)
+        
